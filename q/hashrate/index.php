91222fc5deb20b477996abc6567422160ef3ea04
parsicoin <parsicoin@gmail.com>
2018-10-05 08:21:38 +0330

    api

---
+++ q/hashrate/index.php
@@ -0,0 +1,13 @@
+<?php 
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_URL, 'http://94.156.189.94:18230/getinfo');
+$result = curl_exec($ch);
+$obj = json_decode($result, TRUE);
+curl_close($ch);
+//print_r($obj);
+$difficulty = $obj['difficulty'];
+$hashrate = round($difficulty / 120);
+print_r($hashrate);
+?>
