7b81e8b1a3b999ae58f8e7cd08e9b9ab75403c33
ParsiCoin <parsicoin@gmail.com>
2019-03-02 23:06:18 +0330

    verify signed message

--- config.js
+++ config.js
@@ -3,11 +3,3 @@ var blockTargetInterval = 120;
 var coinUnits = 100000000;
 var symbol = 'PARS';
 var refreshDelay = 30000;
-// pools stats by MainCoins
-var networkStat = {
-    "PARS": [
-		["Pars.Cryptohispano.Net", "https://pars.cryptohispano.net/api/stats"],
-		["Parsicoin.Clevery.Xyz", "https://parsicoin.clevery.xyz/api"],
-		["Parsicoin.MiningPoolHouse.Com", " https://parsicoin.miningpoolhouse.com:4006/stats"]
-    ]
-};
